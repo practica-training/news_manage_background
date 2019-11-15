ad2fae54d3640906adcd51d87be13017c5749d03
荷包蛋 <43177271+GG4EVER@users.noreply.github.com>
2019-11-15 11:01:33 +0800

    新建新闻和类型对应的表

--- src/main/java/com/demo/practical_training/entity/joint_primary_key/NewsAndTypeID.java
+++ src/main/java/com/demo/practical_training/entity/joint_primary_key/NewsAndTypeID.java
@@ -3,9 +3,9 @@ package com.demo.practical_training.entity.joint_primary_key;
 import java.io.Serializable;
 
 /**
- * 新闻标签对应表的复合主键
+ * 新闻和类型对应的复合主键
  */
-public class NewsAndLabelID implements Serializable {
+public class NewsAndTypeID implements Serializable {
     private String newsID;
-    private String labelID;
+    private String typeID;
 }
