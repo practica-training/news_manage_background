e3b66eadb4ded59f93c745a2abdec41d069e67c5
zhong <1491013191@qq.com>
2019-11-19 14:23:41 +0800

    zhong01

--- src/main/java/com/demo/practical_training/manage/web/controller/NewsController.java
+++ src/main/java/com/demo/practical_training/manage/web/controller/NewsController.java
@@ -9,6 +9,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ *
+ */
 @RestController
 @RequestMapping("/manage")
 public class NewsController {
