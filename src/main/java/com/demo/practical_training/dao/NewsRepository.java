be72e111e527e3286c3bf2a95dda0d1486c7658f
panda <pandamessage@qq.com>
2020-01-03 08:36:15 +0800

    修改小bug

--- src/main/java/com/demo/practical_training/dao/NewsRepository.java
+++ src/main/java/com/demo/practical_training/dao/NewsRepository.java
@@ -18,8 +18,8 @@ public interface NewsRepository extends
         JpaRepository<News, String>//分页和排序
 {
     //可以在SQL中增加更多的条件查询
-    @Query(value = "SELECT * FROM news WHERE news.news_state= -1 OR news.news_state= -3",
-            countQuery = "SELECT count(*) FROM news  WHERE news.news_state= -1 OR news.news_state= -3",
+    @Query(value = "SELECT * FROM news WHERE news.news_state = 3",
+            countQuery = "SELECT count(*) FROM news  WHERE news.news_state= 3",
             nativeQuery = true)
     Page<News> findAllPage(Pageable pageable);
     //可以在SQL中增加更多的条件查询
